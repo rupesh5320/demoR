4bfaed79d309ddea68bcec19d572bbea012e941e
rupesh5320 <rupesh2998@gmail.com>
2024-07-11 02:39:21 +0530

    Second Commit

--- src/App.js
+++ src/App.js
@@ -3,8 +3,9 @@ import './App.css';
 
 function App() {
   return (
-    <div className='text-3xl flex justify-center bg-red-400'>
-      Hello World
+    <div className='text-3xl flex-col text-center justify-center'>
+      <p className=" bg-red-400" >Hello World</p> 
+      <p className="bg-green-500">This is Rupesh</p>
     </div>
   );
 }
