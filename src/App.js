80deb06b9e8acf75336da8b413bf37167e8057b1
rupesh5320 <rupesh2998@gmail.com>
2024-07-11 02:31:45 +0530

    This is first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './index.css';
+import './App.css';
+
+function App() {
+  return (
+    <div className='text-3xl flex justify-center bg-red-400'>
+      Hello World
+    </div>
+  );
+}
+
+export default App;
